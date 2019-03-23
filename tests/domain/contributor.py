f7246007fbaa1fbe765fc35a0e118b68931895ca
jorgebertran87 <jorgebertran87@gmail.com>
2019-03-23 18:22:55 +0100

    Create unit test for Contributor VO

---
+++ tests/domain/contributor.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestContributor(unittest.TestCase):
+
+    NAME = 'Contributor'
+    CONTRIBUTOR_CLASSNAME = 'Contributor'
+
+    _contributor = None
+
+    def setUp(self):
+        self._contributor = Contributor(self.NAME)
+
+    def testItReturnsValidContributor(self):
+        self.assertEqual(self._contributor.__class__.__name__, self.CONTRIBUTOR_CLASSNAME)
+
+    def testItReturnsValidContributorName(self):
+        self.assertEqual(self.NAME, self._contributor.name())
