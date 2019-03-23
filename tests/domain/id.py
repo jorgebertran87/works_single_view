4160f51973b278526bb6f27ea90e9b6cc736389c
jorgebertran87 <jorgebertran87@gmail.com>
2019-03-23 13:41:08 +0100

    Create unit test for Id VO

---
+++ tests/domain/id.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestId(unittest.TestCase):
+
+    VALUE = 'Id'
+    ID_CLASSNAME = 'Id'
+
+    _id = None
+
+    def setUp(self):
+        self._id = Id(self.VALUE)
+
+    def testItReturnsValidId(self):
+        self.assertEqual(self._id.__class__.__name__, self.ID_CLASSNAME)
+
+    def testItReturnsValidIdValue(self):
+        self.assertEqual(self.VALUE, self._id.value())
