c82b18a85dda92d355a5b7923371288714291ae2
jorgebertran87 <jorgebertran87@gmail.com>
2019-03-23 13:30:59 +0100

    Create unit tests for Source VO

---
+++ tests/domain/source.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestSource(unittest.TestCase):
+
+    VALUE = 'Source'
+    SOURCE_CLASSNAME = 'Source'
+
+    _source = None
+
+    def setUp(self):
+        self._source = Source(self.VALUE)
+
+    def testItReturnsValidSource(self):
+        self.assertEqual(self._source.__class__.__name__, self.SOURCE_CLASSNAME)
+
+    def testItReturnsValidSourceValue(self):
+        self.assertEqual(self.VALUE, self._source.value())
