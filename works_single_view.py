a365455e2c23d18db6e8f37c7a29fd67ea9f9720
jorgebertran87 <jorgebertran87@gmail.com>
2019-03-25 00:26:29 +0100

    Modify works_single_view script to use Django Rest Framework repositories

--- works_single_view.py
+++ works_single_view.py
@@ -1,20 +1,49 @@
+# -*- coding: utf-8 -*-
+import os
+import django
+from core.infrastructure.api.drf.works_single_view import settings
+#  you have to set the correct path to you settings module
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "core.infrastructure.api.drf.works_single_view.settings")
+
+settings.INSTALLED_APPS.remove('works_single_view.works_single_view')
+settings.INSTALLED_APPS += ('core',)
+
+django.setup()
+
 import click
 from core.infrastructure.cli.load_works_from_csv import LoadWorksFromCSV
-from tests.stubs.work_repository_stub import WorkRepositoryStub
+from core.infrastructure.api.drf.works_single_view.works_single_view.repositories import DjangoWorkRepository
+
 
 @click.command()
 @click.option('--cmd', help='The command to be executed')
 @click.option('--csv', help='The csv path containing works')
 def launcher(cmd: str, csv: str):
-	if cmd == 'load_works_from_csv':
-		print('Loading works from csv...')
-		
-		workRepositoryStub = WorkRepositoryStub()
+    workRepository = DjangoWorkRepository()
 
-		command = LoadWorksFromCSV(csv, workRepositoryStub)
-		command.execute()
+    if cmd == 'load_works_from_csv':
+        print('Loading works from csv...')
 
-		print(str(len(workRepositoryStub.all())) + ' work(s) loaded!')
+        command = LoadWorksFromCSV(csv, workRepository)
+        command.execute()
+
+        print(str(len(workRepository.all())) + ' work(s) loaded!')
+
+    if cmd == 'get_works':
+        print('Getting works...')
+        works = workRepository.all()
+
+        for work in works:
+            print('-------------------')
+            print('-------------------')
+            print('Id: ' + work.id().value())
+            print('Iswc: ' + work.iswc().value())
+            print('Title: ' + work.title().value())
+            print('Source: ' + work.source().value())
+            print('')
+            
 
 if __name__ == '__main__':
     launcher()
